00de8b1e0695946f5c61cf15b7b3239507915fba
Hidde <hiddedv9@gmail.com>
2018-01-16 15:34:06 +0100

    SelectInput en ValidateInput functie toegevoegd.

--- Website shit/portfolio.js
+++ Website shit/portfolio.js
@@ -1,7 +1,15 @@
-function getStuff(){
-	document.querySelector('#test123').onclick=talk;
+function SelectInput(){
+	document.querySelector('#submit').onclick=ValidateInput;
 }
-function talk(){
-alert('Ha gayyyy');
+function ValidateInput(){
+	var x = document.getElementById('oordeelgebruiker').value;
+	if(x=="Zeg iets over Sam!") {
+		alert("Je hebt niks ingevoerd!");
+	}
+	if(x.length <= 4){
+		alert("Vul minstens 5 karakters in!");
+	}
+console.log(x); //voor testen met chrome console.
 }
-window.onload=getStuff;
+
+window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
