1c27a1fcf4720ece70a40c916166973b71756688
Hidde <hiddedv9@gmail.com>
2018-01-16 16:20:51 +0100

    Redirect functies toegevoegd

--- Website shit/portfolio.js
+++ Website shit/portfolio.js
@@ -1,5 +1,7 @@
 function SelectInput(){
 	document.querySelector('#submit').onclick=ValidateInput;
+	document.querySelector('#homeknop').onclick=ShowHome;
+	document.querySelector('#forumknop').onclick=ShowForum;
 }
 function ValidateInput(){
 	var x = document.getElementById('oordeelgebruiker').value;
@@ -12,4 +14,11 @@ function ValidateInput(){
 console.log(x); //voor testen met chrome console.
 }
 
+function ShowForum(){	
+    window.location.replace("file:///C:/Users/Hidde/Documents/GitHub/Kluslus.nl/Website%20shit/forum.html");
+	}
+
+function ShowHome(){	
+    window.location.replace("file:///C:/Users/Hidde/Documents/GitHub/Kluslus.nl/Website%20shit/home.html");
+	}
 window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
