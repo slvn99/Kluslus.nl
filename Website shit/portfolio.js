13f5dc51922a405829f5b512a3fcc5fc76c6bedb
Hidde <hiddedv9@gmail.com>
2018-01-16 16:40:26 +0100

    Begin backend
    Selectinput, Validateinput en redirect functies gemaakt

--- Website shit/portfolio.js
+++ Website shit/portfolio.js
@@ -1,7 +1,24 @@
-function getStuff(){
-	document.querySelector('#test123').onclick=talk;
+function SelectInput(){
+	document.querySelector('#submit').onclick=ValidateInput;
+	document.querySelector('#homeknop').onclick=ShowHome;
+	document.querySelector('#forumknop').onclick=ShowForum;
 }
-function talk(){
-alert('Ha gayyyy');
+function ValidateInput(){
+	var x = document.getElementById('oordeelgebruiker').value;
+	if(x=="Zeg iets over Sam!") {
+		alert("Je hebt niks ingevoerd!");
+	}
+	if(x.length <= 4){
+		alert("Vul minstens 5 karakters in!");
+	}
+console.log(x); //voor testen met chrome console.
 }
-window.onload=getStuff;
+
+function ShowForum(){	
+    window.location.replace("file:///C:/Users/Hidde/Documents/GitHub/Kluslus.nl/Website%20shit/forum.html");
+	}
+
+function ShowHome(){	
+    window.location.replace("file:///C:/Users/Hidde/Documents/GitHub/Kluslus.nl/Website%20shit/home.html");
+	}
+window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
