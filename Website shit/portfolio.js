f65f92c6b44c114528de5b402650430c6230291e
Hidde <hiddedv9@gmail.com>
2018-01-17 13:29:55 +0100

    fixje

--- Website shit/portfolio.js
+++ Website shit/portfolio.js
@@ -1,34 +1,26 @@
+window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
+
 function SelectInput(){
 	document.querySelector('#submit').onclick=ValidateInput;
-	document.querySelector('#homeknop').onclick=ShowHome;
-	document.querySelector('#forumknop').onclick=ShowForum;
-	document.querySelector('#uitlogknop').onclick=LogOut;
-	document.querySelector('#mijnberichtenknop').onclick=ShowMessages;
+	/*document.querySelector('#homeknop').onclick=ShowHome;
+	//document.querySelector('#forumknop').onclick=ShowForum;
+	//document.querySelector('#uitlogknop').onclick=LogOut;
+	//document.querySelector('#mijnberichtenknop').onclick=ShowMessages;
 	document.querySelector('#profielknop').onclick=ShowProfile;
+	dit is miss later nog nodig ofzo*/
 }
 function ValidateInput(){
 	var x = document.getElementById('oordeelgebruiker').value;
 	if(x=="Zeg iets over Sam!") {
 		alert("Je hebt niks ingevoerd!");
 	}
-	if(x.length <= 4){
+	else if(x.length <= 4){
 		alert("Vul minstens 5 karakters in!");
+	}
+
+	else{
+		alert("Bedankt! Je beoordeling " + x + " is opgeslagen.")
 	}
 console.log(x); //voor testen met chrome console.
 }
 
-function ShowForum(){	
-   
-}
-
-function ShowHome(){	
-
-}
-function LogOut{
-
-}	
-
-Function ShowMessages(){
-
-}
-window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
