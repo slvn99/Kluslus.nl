10a54c14503460d26337e2b473a4cd63c9b8574c
Hidde <hiddedv9@gmail.com>
2018-01-18 15:40:14 +0100

    comments

--- Website shit/portfolio.js
+++ Website shit/portfolio.js
@@ -1,13 +1,7 @@
 window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
-var counter = 1;
+var counter = 1; // counter voor dynamische benaming van reacties.
 function SelectInput(){
 	document.querySelector('#submit').onclick=ValidateInput, addreactie;
-	/*document.querySelector('#homeknop').onclick=ShowHome;
-	//document.querySelector('#forumknop').onclick=ShowForum;
-	//document.querySelector('#uitlogknop').onclick=LogOut;
-	//document.querySelector('#mijnberichtenknop').onclick=ShowMessages;
-	document.querySelector('#profielknop').onclick=ShowProfile;
-	dit is miss later nog nodig ofzo*/
 }
 function ValidateInput(){
 	var text = document.getElementById('oordeelgebruiker').value;
@@ -24,12 +18,7 @@ function ValidateInput(){
 	}
 }
 
-/*function DisplayImage(){
-var par = document.getElementById("imagebox");
-var img = document.createElement("image");
-img.src = "Resources/sam.jpg";
-par.appendChild(img);
-}*/
+
 
 function addreactie() {
 var text = document.getElementById('oordeelgebruiker').value;
@@ -40,27 +29,12 @@ var label = document.createElement("Label");
 var artname = document.createElement('h1');
 label.innerHTML = text;   
 artname.innerHTML = name;  
-console.log(section);
 newart.setAttribute("ID",("reactie"+ counter));
 newart.setAttribute("style","float:center;border-radius: 5px;border:solid red 1px;margin:15px;");
-/*newart.setAttribute("style","border radius 5px;");	
-newart.setAttribute("style","margin-top 20px;");
-newart.setAttribute("style","padding 15px;");	
-newart.setAttribute("style","box-shadow 5px 5px;");	*/
-console.log(newart);
 newart.appendChild(artname);
 section.appendChild(newart);
 newart.appendChild(label);
-
-
-
-
-
-//var foo = document.getElementById("reacties");
-
-
-//foo.appendChild(label);
-//foo.appendChild(element);
-//alert("Bedankt! Je beoordeling "+text+" is opgeslagen.");
+//nu zouden die variabelen dus gesaved moeten worden naar een db oid. of alleen section variable geen idee.
+alert("Je reactie "+text+" is opgeslagen!");
 counter++;
 }
