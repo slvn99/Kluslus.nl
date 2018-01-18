894884fb64f0079c82fa2d68e8191bad45bfd175
Sam <slvn99@gmail.com>
2018-01-18 16:12:58 +0100

    media player own skin (wip)

--- Website shit/home.js
+++ Website shit/home.js
@@ -1,3 +1,49 @@
+function doFirst(){
+	barSize=430;
+	myMovie=document.getElementById('myMovie');
+	playButton=document.getElementById('playButton');
+	bar=document.getElementById('defaultBar');
+	progressBar=document.getElementById('progressBar');
+
+	playButton.addEventListener('click', playOrPause, false);
+	bar.addEventListener('click', clickedBar, false);
+}
+
+function playOrPause(){
+	if(!myMovie.paused && !myMovie.ended){
+		myMovie.pause();
+		playButton.innerHTML='Play';
+		window.clearInterval(updateBar);
+	}
+	else{
+		myMovie.play();
+		playButton.innerHTML='Pause';
+		updateBar=setInterval(update, 500);
+	}
+}
+
+function update(){
+	if(!myMovie.ended){
+		var size=parseInt(myMovie.currentTime*barSize/myMovie.duration);
+		progressBar.style.width=size+'px';
+	}
+	else{
+		progressBar.style.width='0px';
+		playButton.innerHTML='play';
+		window.clearInterval(updateBar);	
+	}
+}
+function clickedBar(e){
+	if(!myMovie.paused && !myMovie.ended){
+		var mouseX=e.pageX-bar.offsetLeft;
+		var newtime=mouseX*myMovie.duration/barSize;
+		myMovie.currentTime=newtime;
+		progressBar.style.width=mouseX+'px';
+	}
+}
+
+window.addEventListener('load', doFirst, false);
+
 function getStuff(){
 	document.querySelector('#logo').onclick=talk;
 }
