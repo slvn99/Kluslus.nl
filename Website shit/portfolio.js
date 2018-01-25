1e62d780965992e26e6d19d9405982e1ce2e3292
Sam <slvn99@gmail.com>
2018-01-25 13:27:24 +0100

    changes?

--- Website shit/portfolio.js
+++ Website shit/portfolio.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-function SelectInput(){
-	document.querySelector('#submit').onclick=ValidateInput;
-	document.querySelector('#homeknop').onclick=ShowHome;
-	document.querySelector('#forumknop').onclick=ShowForum;
-	document.querySelector('#uitlogknop').onclick=LogOut;
-	document.querySelector('#mijnberichtenknop').onclick=ShowMessages;
-	document.querySelector('#profielknop').onclick=ShowProfile;
-	document.querySelector('#portfoliocreateknop').onclick=ShowPortfoliocreate
-}
-function ValidateInput(){
-	var x = document.getElementById('oordeelgebruiker').value;
-	if(x=="Zeg iets over Sam!") {
-		alert("Je hebt niks ingevoerd!");
-	}
-	if(x.length <= 4){
-		alert("Vul minstens 5 karakters in!");
-	}
-console.log(x); //voor testen met chrome console.
-}
-
-function ShowForum(){	
-   
-}
-
-function ShowHome(){	
-
-}
-function LogOut{
-
-}	
-
-Function ShowMessages(){
-
-}
-window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
-=======
 window.onload=SelectInput; // voer deze functie uit wanneer de pagina geladen word.
 var counter = 1; // counter voor dynamische benaming van reacties.
 function SelectInput(){
@@ -88,4 +51,3 @@ function saveReactie(text,name){
 
 
 }
->>>>>>> Back-end-home
